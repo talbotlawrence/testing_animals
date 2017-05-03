20454bb3339abb5b358f4815456c62b93c0e64d3
Talbot <talbotlawrence75@gmail.com>
2017-05-02 20:10:34 -0500

    getting a better understanding of the __init__ method

--- TestAnimals.py
+++ TestAnimals.py
@@ -5,20 +5,36 @@ class TestTheAnimals(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.animal = Animal('Buddy', 'Panda')
+        """
+        This method instantiates an Animal/Panda and a Dog
+        These objects are available for the test methods below
+        """
+
+        self.animal = Animal('Buddy', 'Panda', 4)
         self.angus = Dog('Angus')
         print(self.animal.name)
 
     def test_animal_name_property_has_correct_value(self):
-        self.assertEqual(self.animal.name, 'Buddy')
+        """the line below implies that the Animal Class will have a NAME getter and setter"""
+        self.assertEqual(self.animal.name, 'Buddy')   
 
     def test_animal_species_property_has_correct_value(self):
+        """the line below implies that the Animal Class will have a SPECIES getter and setter"""
         self.assertEqual(self.animal.species, 'Panda')
 
     def test_dog_species_property_has_correct_value(self):
+        """the line below implies that there will be a Dog Class"""
         self.assertEqual(self.angus.species, 'Dog')
 
     def test_walk_method_sets_speed(self):
+        """
+        This test method will set the number of legs to 4.
+        It implies that there will be a set_legs() method in the Animal Class.
+        It also implies that there will be a walk() method in the Animal Class.
+        The Animal class __init__ method will set the speed.
+        The Dog class will modify the speed.
+        """
+
         legs = 4
         self.animal.set_legs(legs)
         self.animal.walk()
--- animals.py
+++ animals.py
@@ -1,18 +1,18 @@
 class Animal:
-    def __init__(self, name, species):
+    def __init__(self, name, species, legs):      #why doesn't speed and legs go in here?
         self.__name = name
         self.__species = species
         self.speed = 0
         self.legs = 0
 
+    #NAME and SPECIES getters and setters############################################
     @property
     def name(self):
         return self.__name
 
     @name.setter
     def name(self, val):
-        if isinstance(val, str):
-            self.__name = val
+        self.__name = val
 
     @property
     def species(self):
@@ -22,26 +22,40 @@ class Animal:
     def species(self, val):
         self.__species = val
 
+    #################################################################################
+
+    #Animal Methods##################################################################
+
     def walk(self):
-        """Sets the speed of the animal"""
-        if isinstance(self.legs, int) and self.legs > 0:
-            self.speed = self.speed + (0.1 * self.legs)
-        else:
-            raise ValueError('Legs property must contain a number greater than 0')
+        """Sets the speed"""
+        try:
+            if self.legs > 0:
+                self.speed = self.speed + (0.1 * self.legs)
+        except ValueError:
+            print('Legs property must contain a number greater than 0')
 
     def set_legs(self, number_of_legs):
-        """Sets the species of the animal"""
+        """Sets the number of legs"""
         self.legs = number_of_legs
-
 
     def __str__(self):
         return "{} is a {}".format(self.name, self.species)
 
+    #################################################################################
 
 class Dog(Animal):
     def __init__(self, name):
-        Animal.__init__(self, name, "Dog")
+        Animal.__init__(self, name, "Dog", 4)     #I don't like having 4 in two different places
+        self.__name = name
+        #self.speed = 0.2
+        self.legs = 4                             #This is the line that is working, not line 48
 
     def walk(self):
         """Sets the speed of the dog"""
         self.speed = self.speed + (0.2 * self.legs)  
+
+charlie = Dog('charlie')
+print("My dog is named {} and he has {} legs".format(charlie.name.title(), charlie.legs))
+plato = Dog('plato')
+print("My dog is named {} and this is his speed: {}".format(plato.name.title(), plato.walk()))
+print(plato.legs)
